aaee0b8870f87cf1c8c6d3cd7cd1e99fa05c7456
jarred richards <jarred2828@gmail.com>
2019-08-08 17:06:54 -0400

    EverythingButPUT

--- channels/channels-router.js
+++ channels/channels-router.js
@@ -57,6 +57,28 @@ router.post('/', (req, res) => {
 
 //
 
+router.get('/:id', (req, res) => {
+  const id = req.params.id
+
+  Posts.findById(id)
+  .then((posts) => {
+
+    if(posts.length === 0) {
+      res.status(404).json({ message: "The post with the specified ID does not exist." })
+    } else {
+      res.status(200).json(posts)
+    }
+  })
+  .catch(err => {
+    res.status(500).json({ 
+      error: "The post information could not be retrieved.",
+      err
+    })
+  })
+})
+
+//
+
 router.get('/:id/comments', (req, res) => {
   const id = req.params.id
 
